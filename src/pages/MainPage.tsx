44b11a15c8b186e55422643a2c752570b66c34e0
Jungwoo <evencoding@gmail.com>
2024-02-14 22:18:50 +0900

    chore: 불필요한 key 제거

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -24,43 +24,44 @@ export function MainPage() {
   const hasNoContent = !data?.documents.length;
 
   return (
-    <main className="flex w-full max-w-[880px] flex-col p-4 md:p-8 lg:p-12">
-      <SEO title="Documents" description="모든 문서" image="" />
-      <div className="flex flex-col">
-        <div className="mb-4 flex justify-between">
-          <Txt className="border-none text-4xl font-extrabold">📄 문서</Txt>
-          <CreateDocumentMenu />
+    <div className="flex w-full flex-col p-4 md:p-8 lg:p-12">
+      <main className="flex w-full max-w-[880px] flex-col">
+        <SEO title="Documents" description="모든 문서" image="" />
+        <div className="flex flex-col">
+          <div className="mb-4 flex justify-between">
+            <Txt className="border-none text-4xl font-extrabold">📄 문서</Txt>
+            <CreateDocumentMenu />
+          </div>
+          <Txt typography="small" className="text-foreground/60">
+            문서가 많을수록 오늘의 퀴즈가 다채로워져요!
+          </Txt>
         </div>
-        <Txt typography="small" className="text-foreground/60">
-          문서가 많을수록 오늘의 퀴즈가 다채로워져요!
-        </Txt>
-      </div>
-      {data?.documents.map((document) => (
-        <div key={document.id} className="mt-8 space-y-4">
-          <article
-            key={document.id}
-            id={String(document.id)}
-            className="cursor-pointer rounded-lg border-2 p-4 transition-all hover:bg-foreground/5"
-            onClick={moveToDetail}
-          >
-            <div className="flex items-center justify-between">
-              <Txt typography="large">{document.documentName}</Txt>
-              <Txt typography="small" className="text-foreground/40">
-                {formatDate(document.createdAt)}
-              </Txt>
-            </div>
-            {document.summary && (
-              <Txt className="mt-[-20px] line-clamp-2 text-sm font-medium text-foreground/80">{document.summary}</Txt>
-            )}
-          </article>
-        </div>
-      ))}
-      {hasNoContent && (
-        <div className="mt-24 flex w-full flex-col items-center justify-center space-y-2 font-semibold text-foreground/50">
-          <Txt>생성된 문서가 없어요</Txt>
-          <Txt>문서를 업로드하시면 매일 새로운 퀴즈를 생성해서 알림을 보내드릴게요 🚀</Txt>
-        </div>
-      )}
-    </main>
+        {data?.documents.map((document) => (
+          <div key={document.id} className="mt-8 space-y-4">
+            <article
+              id={String(document.id)}
+              className="cursor-pointer rounded-lg border-2 p-4 transition-all hover:bg-foreground/5"
+              onClick={moveToDetail}
+            >
+              <div className="flex items-center justify-between">
+                <Txt typography="large">{document.documentName}</Txt>
+                <Txt typography="small" className="text-foreground/40">
+                  {formatDate(document.createdAt)}
+                </Txt>
+              </div>
+              {document.summary && (
+                <Txt className="mt-[-20px] line-clamp-2 text-sm font-medium text-foreground/80">{document.summary}</Txt>
+              )}
+            </article>
+          </div>
+        ))}
+        {hasNoContent && (
+          <div className="mt-24 flex w-full flex-col items-center justify-center space-y-2 font-semibold text-foreground/50">
+            <Txt>생성된 문서가 없어요</Txt>
+            <Txt>문서를 업로드하시면 매일 새로운 퀴즈를 생성해서 알림을 보내드릴게요 🚀</Txt>
+          </div>
+        )}
+      </main>
+    </div>
   );
 }
