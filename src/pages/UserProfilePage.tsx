04f0d34401d121935175b1b2ca35da3a77ddd377
Jungwoo <evencoding@gmail.com>
2024-02-15 21:59:50 +0900

    fix: md 에디터에 tailwind 초기화로 li marker 안보이는 거 수정

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -61,8 +61,10 @@ export function UserProfilePage() {
           </div>
         </div>
       </div>
-      <div className="fixed bottom-5 left-1/2 w-full max-w-3xl -translate-x-1/2 px-5">
-        <Txt typography="small">비즈니스 문의사항은 support@picktoss.com</Txt>
+      <div className="fixed bottom-5 left-1/2 w-full max-w-3xl -translate-x-1/2 space-y-2 px-5">
+        <Txt typography="small" className="text-foreground/60">
+          비즈니스 문의사항: support@picktoss.com
+        </Txt>
         <Button className="w-full" onClick={() => push('/feedback')}>
           피드백 및 문의
         </Button>
