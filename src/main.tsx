1b8e9d0b52adbff556f927c69f3ed64fa82c6d4a
Jungwoo <evencoding@gmail.com>
2024-02-16 07:40:34 +0900

    feat: ga4 페이지 추적을 위한 RouteChangeTracker 구현

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,6 @@ import App from './App.tsx';
 import './index.css';
 import { Toaster } from './components/ui/toaster.tsx';
 
-import ReactGA from 'react-ga4';
-
-if (import.meta.env.VITE_GOOGLE_ANALYTICS_ID) {
-  ReactGA.initialize(import.meta.env.VITE_GOOGLE_ANALYTICS_ID);
-}
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Toaster />
--- src/router/components/Root.tsx
+++ src/router/components/Root.tsx
@@ -2,10 +2,12 @@ import { Center } from '@/components/shared/Center';
 import { Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { Outlet } from 'react-router-dom';
+import { RouteChangeTracker } from './RouteChangeTracker';
 
 export function Root() {
   return (
     <div className="min-h-screen">
+      <RouteChangeTracker />
       <ErrorBoundary fallback={<Center>알 수 없는 에러가 발생했어요, 잠시 후 다시 시도해주세요</Center>}>
         <Suspense fallback={<Center>Loading...</Center>}>
           <Outlet />
