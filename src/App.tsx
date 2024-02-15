e809d1e572e24f7db3d5bd01c57adde826b2f450
Jungwoo <evencoding@gmail.com>
2024-02-16 07:13:15 +0900

    feat: GA 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './router/routerConfig';
 import { TanstackProvider } from './providers/TanstackProvider';
 import { HelmetProvider } from 'react-helmet-async';
+import ReactGA from 'react-ga';
+
+const gaTrackingId = import.meta.env.VITE_GOOGLE_ANALYTICS_ID;
+ReactGA.initialize(gaTrackingId);
 
 function App() {
   return (
