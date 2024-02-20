7bdf1c00d5947e6c37fc996add55884fdd6cf055
Jungwoo <evencoding@gmail.com>
2024-02-20 09:38:08 +0900

    refactor(SidebarLayout): 불필요한 css 제거

--- src/router/components/SidebarLayout.tsx
+++ src/router/components/SidebarLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 
 export function SidebarLayout() {
   return (
-    <div className="flex flex-col items-center justify-center sm:flex-row sm:items-start">
+    <div className="flex flex-col sm:flex-row sm:justify-center">
       <Category />
       <Outlet />
     </div>
