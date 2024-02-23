6834b0674a43dc624d42a12fca90ed57b5ca0859
Jungwoo <evencoding@gmail.com>
2024-02-23 19:34:45 +0900

    design: 모바일 badge 스타일 변경

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -63,7 +63,7 @@ export function Nav() {
             <Menu />
           </DropdownMenuTrigger>
           <DropdownMenuContent>
-            <DropdownMenuLabel className={isPro ? 'bg-point' : 'bg-foreground/30'}>
+            <DropdownMenuLabel className={isPro ? 'bg-point' : ''}>
               <PlanBadge isPro={isPro} className="w-full py-0" />
             </DropdownMenuLabel>
             <DropdownMenuSeparator />
